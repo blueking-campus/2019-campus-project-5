2ebd9f32045b107b1075b35c479a5f1746315fca
BreakCup <haoyuan_zhuhai@outlook.com>
2019-04-28 21:45:46 +0800

    编写首页view

--- home_application/views.py
+++ home_application/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from common.mymako import render_mako_context
+from home_application.models import Award
 
 
 # 开发框架中通过中间件默认是需要登录态的，如有不需要登录的，可添加装饰器login_exempt【装饰器引入from account.decorators import login_exempt】
@@ -7,6 +8,8 @@ def home(request):
     """
     首页
     """
+    viewable_awards = Award.objects.all_by_username(request.user)
+    print viewable_awards
     return render_mako_context(request, '/home_application/home.html')
 
 
