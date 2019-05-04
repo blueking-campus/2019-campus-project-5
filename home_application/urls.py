16333161d6872c48364d66ccd1f69bd308f1b582
BreakCup <haoyuan_zhuhai@outlook.com>
2019-05-05 00:03:01 +0800

    fix:
    首页导航bug

--- home_application/urls.py
+++ home_application/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns(
     'home_application.views',
     # 首页--your index
     (r'^$', 'home'),
-    (r'^dev_guide/$', 'dev_guide'),
-    (r'^contact/$', 'contact'),
+    (r'^manage/$', 'manage'),
+    (r'^personal/$', 'personal'),
 )
