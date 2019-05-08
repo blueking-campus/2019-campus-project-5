138c253119c2cebf8858d2bd5f59e58d3406f284
BreakCup <haoyuan_zhuhai@outlook.com>
2019-05-08 20:04:21 +0800

    feat: 日期校验和整数校验

--- home_application/decorators.py
+++ home_application/decorators.py
@@ -2,6 +2,10 @@
 from functools import wraps
 
 from django.http import HttpResponse
+from django.utils.decorators import available_attrs
+
+from home_application.utils import is_int
+from home_application.utils import is_vaild_datetime
 
 
 def require_superuser(func):
@@ -13,3 +17,47 @@ def require_superuser(func):
         else:
             return HttpResponse('superuser required!')
     return wrapper
+
+
+def require_int_GET(func, name):
+
+    @wraps(func, name)
+    def wrapper(request, *args, **kwargs):
+        data = request.GET.get(name)
+        if is_int(data) or (data is None):
+            return func(request, *args, **kwargs)
+        else:
+            return HttpResponse(name + '需要为整数，或者不上传该参数。')
+    return wrapper
+
+def require_int_GET(name):
+    """
+    传入整数或者不传入
+    name: 验证的变量名
+    """
+    def decorator(func):
+        @wraps(func, assigned=available_attrs(func))
+        def inner(request, *args, **kwargs):
+            data = request.GET.get(name)
+            if is_int(data):
+                return func(request, *args, **kwargs)
+            else:
+                return HttpResponse(name + '需要为整数')
+        return inner
+    return decorator
+
+def require_datetime_GET(name):
+    """
+    传入整数或者不传入
+    name: 验证的变量名
+    """
+    def decorator(func):
+        @wraps(func, assigned=available_attrs(func))
+        def inner(request, *args, **kwargs):
+            data = request.GET.get(name)
+            if (not is_vaild_datetime(data)) and data:
+                # datetime不为空且不是datetime格式
+                return HttpResponse('日期格式需要为：%Y-%m-%d %H:%M:%S，或者不传入该参数')
+            return func(request, *args, **kwargs)
+        return inner
+    return decorator
