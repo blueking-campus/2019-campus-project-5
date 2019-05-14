d4b481f0f9b6d445fd4dcf8f42897dfd99475b44
BreakCup <haoyuan_zhuhai@outlook.com>
2019-05-14 15:37:41 +0800

    refactor: 完善错误请求处理方式

--- home_application/decorators.py
+++ home_application/decorators.py
@@ -3,6 +3,7 @@ from functools import wraps
 
 from django.http import HttpResponse
 from django.utils.decorators import available_attrs
+from common.mymako import render_mako_context
 
 from home_application.utils import is_int
 from home_application.utils import is_vaild_datetime
@@ -15,7 +16,7 @@ def require_superuser(func):
         if request.user.is_superuser:
             return func(request, *args, **kwargs)
         else:
-            return HttpResponse('superuser required!')
+            return render_mako_context(request, '403.html')
     return wrapper
 
 
@@ -27,7 +28,7 @@ def require_int_GET(func, name):
         if is_int(data) or (data is None):
             return func(request, *args, **kwargs)
         else:
-            return HttpResponse(name + '需要为整数，或者不上传该参数。')
+            return HttpResponse(name + '需要为整数，或者不上传该参数。', status=400)
     return wrapper
 
 def require_int_GET(name):
@@ -42,7 +43,7 @@ def require_int_GET(name):
             if is_int(data):
                 return func(request, *args, **kwargs)
             else:
-                return HttpResponse(name + '需要为整数')
+                return HttpResponse(name + '需要为整数', status=400)
         return inner
     return decorator
 
@@ -57,7 +58,7 @@ def require_datetime_GET(name):
             data = request.GET.get(name)
             if (not is_vaild_datetime(data)) and data:
                 # datetime不为空且不是datetime格式
-                return HttpResponse('日期格式需要为：%Y-%m-%d %H:%M:%S，或者不传入该参数')
+                return HttpResponse('日期格式需要为：%Y-%m-%d %H:%M:%S，或者不传入该参数',status=400)
             return func(request, *args, **kwargs)
         return inner
     return decorator
